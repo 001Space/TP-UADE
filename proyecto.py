2fcd12d5240b4567c9587592c075d232a2245c76
giu cataldi <spaceof.hell@gmail.com>
2024-08-25 18:28:15 -0300

    Uploading starting file

---
+++ proyecto.py
@@ -0,0 +1,6 @@
+#Hacer un login usurarios/administradores
+#Buscador de mercaderia
+#Dentro del administrador, modificar y agregar productos, stock, stock en sucursarles
+#Vendedores, sumar restar stock, carrito de compras, ejecutar carrito (suma de costo, resta de stock)
+#Comision vendedores
+
